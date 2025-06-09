69c3a7abf94d96e3c8ccdc31d42ab732599a2773
Yasmim <125297996+YasmimHiss@users.noreply.github.com>
2025-06-09 11:49:22 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
 
-        for(let j=0;j<botoes.length;j++){
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
+}
+
+const contadores = document.querySelectorAll(".contador");
+
+const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
+
+
+contadores[0].textContent = caucula (tempoObjetivo1);
+
+funtion cauculatempo(tempoobjetivo){
+    let tempoAtual = new Date();
+    let tempofinal = tempoobjetivo - tempoAtual;
+    let temposegundos/1000;
+    return segundos;
 }
